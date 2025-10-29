957c671eae137410c9e03bf5939b5c49867d5914
Aliza's <aliza.visuals@gmail.com>
2025-10-29 10:26:50 -0500

    Main Added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Asset> myAsset = new ArrayList<>();
+
+        myAsset.add( new House("My House", "2020-05-10",20000, "34 Park Lane", 2, 3999));
+
+        myAsset.add(new Vehicle("Jane's Truck", "2023-02-11", 35000, "Toyota", 2019,120000));
+
     }
 }
